37a31a72c88c196d05afe594c445c0b3ac9f6a89
Christian Tiovanto <christiantiovanto1@gmail.com>
2024-11-02 10:01:08 +0700

    fix: fix error controller for mongoose duplicate error

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -6,7 +6,7 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
@@ -90,7 +90,6 @@ export function globalErrorHandler(err, req, res, next) {
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
     error.message = err.message;
-
     if (error.name === "CastError") error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === "ValidationError")
