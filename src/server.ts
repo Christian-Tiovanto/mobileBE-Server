e822467ad65949f075806aa2b17369dc78d1a37b
Christian Tiovanto <christiantiovanto1@gmail.com>
2024-11-20 15:56:59 +0700

    bump: v0.0.1-beta.1

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,34 @@ import { configDotenv } from "dotenv";
 import { join } from "path";
 import mongoose from "mongoose";
 import app from "./app";
-configDotenv({ path: join(__dirname, "..", ".env") });
-const PORT = process.env.PORT as any;
-mongoose
-  .connect(`mongodb://localhost:27017/mobileBE?directConnection=true`)
-  .then(async () => {
-    console.log("DB Connection Successful");
+if ((process.env.NODE_ENV = "development")) {
+  configDotenv({ path: join(__dirname, "..", ".env") });
+  const PORT = process.env.PORT as any;
+  mongoose
+    .connect(`mongodb://localhost:27017/mobileBE?directConnection=true`)
+    .then(async () => {
+      console.log("DB Connection Successful");
 
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`listening to port ${PORT}`);
+      app.listen(PORT, "0.0.0.0", () => {
+        console.log(`listening to port ${PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+} else if ((process.env.NODE_ENV = "production")) {
+  configDotenv({ path: join(__dirname, "..", ".env.prod") });
+  const PORT = process.env.PORT as any;
+  mongoose
+    .connect(process.env.DB_URL)
+    .then(async () => {
+      console.log("DB Connection Successful");
+
+      app.listen(PORT, "0.0.0.0", () => {
+        console.log(`listening to port ${PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
