2764fa2a056b24c4a3b048721b6a417e00c0bd0d
Christian-Tiovanto <christiantiovanto1@gmail.com>
2024-12-05 22:45:00 +0700

    remove env and prod environemnt

--- src/server.ts
+++ src/server.ts
@@ -2,38 +2,36 @@ import { configDotenv } from "dotenv";
 import { join } from "path";
 import mongoose from "mongoose";
 import app from "./app";
-console.log(process.env.NODE_ENV);
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV == "development") {
-  configDotenv({ path: join(__dirname, "..", ".env") });
-  console.log("maosk sini?");
-  const PORT = process.env.PORT as any;
-  mongoose
-    .connect(`mongodb://localhost:27017/mobileBE?directConnection=true`)
-    .then(async () => {
-      console.log("DB Connection Successful");
+configDotenv({ path: join(__dirname, "..", ".env") });
+console.log("maosk sini?");
+const PORT = process.env.PORT as any;
+mongoose
+  .connect(`mongodb://localhost:27017/mobileBE?directConnection=true`)
+  .then(async () => {
+    console.log("DB Connection Successful");
 
-      app.listen(PORT, "0.0.0.0", () => {
-        console.log(`listening to port ${PORT}`);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`listening to port ${PORT}`);
     });
-} else if (process.env.NODE_ENV == "production") {
-  console.log("proddd");
-  configDotenv({ path: join(__dirname, "..", ".env.prod") });
-  const PORT = process.env.PORT as any;
-  mongoose
-    .connect(process.env.DB_URL)
-    .then(async () => {
-      console.log("DB Connection Successful");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+// if (process.env.NODE_ENV == "development") {
+// } else if (process.env.NODE_ENV == "production") {
+//   console.log("proddd");
+//   configDotenv({ path: join(__dirname, "..", ".env.prod") });
+//   const PORT = process.env.PORT as any;
+//   mongoose
+//     .connect(process.env.DB_URL)
+//     .then(async () => {
+//       console.log("DB Connection Successful");
 
-      app.listen(PORT, "0.0.0.0", () => {
-        console.log(`listening to port ${PORT}`);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+//       app.listen(PORT, "0.0.0.0", () => {
+//         console.log(`listening to port ${PORT}`);
+//       });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
